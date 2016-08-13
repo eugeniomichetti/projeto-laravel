9542dbb8bfa98efe07de72376dbdee1b4da312c5
eugeniomichetti <suporte@larimoveis.com.br>
2016-08-13 13:43:30 -0300

    Adicionado seed para OAuthClient

---
+++ database/seeds/OAuthClientSeeder.php
@@ -0,0 +1,24 @@
+<?php
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class OAuthClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('oauth_clients')->insert([
+            [
+                'id' => 'appid1',
+                'secret' => 'secretapp',
+                'name' => 'ProjetoLaravel',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]
+        ]);
+    }
+}
